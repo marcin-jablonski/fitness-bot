f2407db681e5f9ea94ee5f090133335b79e8adfc
Marcin Jabłoński <marcin.k.jablonski@gmail.com>
2020-04-08 20:22:17 +0200

    Persistent trainings

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const config = require("./config.json");
 const { Pool } = require('pg');
 const yn = require("yn");
 const moment = require("moment-timezone");
+const _ = require("lodash");
 
 require("dotenv").config();
 
@@ -26,6 +27,31 @@ const logger = createLogger({
 });
 
 logger.level = 'debug';
+
+var queuedTrainings = [];
+
+async function notifyAboutTraining(trainingId) {
+  logger.debug("Notifying about training");
+
+  db.query("SELECT * FROM trainings WHERE id = $1", [trainingId])
+    .then(res => {
+      client.channels
+        .fetch(res.rows[0].channel)
+        .then(channel => {
+          channel.send("Hey, training time! @everyone");
+
+          logger.debug("Training link: " + res.rows[0].link);
+    
+          if (res.rows[0].link !== null && res.rows[0].link !== undefined) {
+            channel.send(res.rows[0].link);
+          }
+          
+          _.remove(queuedTrainings, (item) => item === trainingId);
+          db.query("UPDATE trainings SET completed = TRUE WHERE id = $1", [trainingId]).catch(err => { throw err; });
+        });      
+    })
+    .catch(err => { throw err; });
+}
 
 const client = new discord.Client();
 
@@ -114,13 +140,16 @@ client.on('message', async (message) => {
 
       message.channel.send("Training set for " + trainingDate.toString());
 
-      setTimeout(async function() {
-        logger.debug("Notifying about training");
-        message.channel.send("Hey, training time! @everyone");
-        if (trainingLink !== null) {
-          message.channel.send(trainingLink);
+      db.query("INSERT INTO trainings(channel, date, link) VALUES ($1, $2, $3) RETURNING *;", [message.channel.id, trainingDate, trainingLink])
+        .then(res => {
+          if (trainingDate < moment().startOf("hour").hour(moment().startOf("hour").hour() + 1)) {
+          logger.debug("Training starts within current hour, starting timeout");
+
+          queuedTrainings.push(res.rows[0].id);
+          setTimeout(notifyAboutTraining.bind(null, res.rows[0].id), milisToTraining)
         }
-      }, milisToTraining)
+        })
+        .catch(err => { throw err; });
 
       break;
     default:
@@ -131,3 +160,36 @@ client.on('message', async (message) => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+const nextFullHour = moment().startOf("hour").hour(moment().startOf("hour").hour() + 1);
+
+setTimeout(() => {
+  setInterval(() => {
+    db.query("SELECT * FROM trainings WHERE completed = FALSE")
+      .then(res => {
+        res.rows
+          .filter(
+            (item) => (item.date <= moment().startOf("hour").hour(moment().startOf("hour").hour() + 1)) && 
+            (queuedTrainings.findIndex((qTrainingId) => qTrainingId === item.id) === -1)
+          )
+          .forEach(training => {
+            logger.debug("Queueing training: " + JSON.stringify(training));
+            queuedTrainings.push(training.id);
+            const milisToTraining = moment(training.date).diff(moment());
+            setTimeout(notifyAboutTraining.bind(null, training.id), milisToTraining);
+          })
+      })
+  }, 1000 * 60 * 60)
+}, nextFullHour.diff(moment()))
+
+db.query("SELECT * FROM trainings WHERE completed = FALSE")
+  .then(res => {
+    res.rows
+      .filter((item) => item.date <= nextFullHour)
+      .forEach(training => {
+        logger.debug("Queueing training: " + JSON.stringify(training));
+        queuedTrainings.push(training.id);
+        const milisToTraining = moment(training.date).diff(moment());
+        setTimeout(notifyAboutTraining.bind(null, training.id), milisToTraining);
+      })
+  })
